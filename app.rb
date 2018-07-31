f4db169ce60ad7f635dcc6575a31455090bf8e12
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-07-31 14:31:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get "/" do
-    "Welcome to my app!! "
+    "Welcome to my app!! My name is... why would i tell you? You might be a creep!"
   end
 
 end
